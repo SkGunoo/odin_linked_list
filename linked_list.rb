6e951c9aa7ac36afa2228c754169ed055215f3c7
Gunoo Kim <73500662+SkGunoo@users.noreply.github.com>
2025-06-10 09:33:45 +0100

    feat: add prepent method to LinkedList class

--- linked_list.rb
+++ linked_list.rb
@@ -7,21 +7,40 @@ class LinkedList
     @tail = nil
   end
 
+  #add node to end of the list
   def append(value)
-    last_node = @head 
+    last_node = @head
     new_node = Node.new(value)
     new_node.next_node = @tail
 
+    #if there is no node in linkedlist just add a node
     if last_node == nil 
       @head = new_node
     else
+      #travel to the last 'node' not tail 
       until last_node.next_node == nil
         last_node = last_node.next_node
       end 
       last_node.next_node = new_node
     end
-
   end
+
+  def prepend(value)
+    first_node = @head
+    new_node = Node.new(value)
+
+    #when linked list is empty
+    if first_node.nil?
+      @head = new_node
+      @head.next_node = @tail 
+
+    #when there are nodes  
+    else
+      new_node.next_node = @head
+      @head = new_node
+    end
+  end
+
 end
 
 
@@ -42,8 +61,8 @@ list = LinkedList.new
 
 list.append("test")
 list.append("test2")
-list.append("test3")
-list.append("test4")
+list.prepend("haha")
+list.prepend("works")
 
 
 p list.head
